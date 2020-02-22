d9c8a20fb5fcb1b21a2731a06f4b573d9d6160b0
andrewzhbj <42502197+andrewzhbj@users.noreply.github.com>
2020-02-22 19:51:58 -0300

    operadores IN, NOT IN

---
+++ Teoría/Operadores de strings.py
@@ -0,0 +1,10 @@
+"""
+	Operador IN			> Si existe en .. devuelve True
+	Operador NOT IN		> No existe en .. devuelve False
+
+"""
+
+Mensaje = "Cave es pelotudo"
+
+print("cave" in Mensaje)
+print("cave" not in Mensaje)
