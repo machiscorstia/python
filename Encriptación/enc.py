caa49b18c527ff513c02a2f60032e069046d2223
Manuel Cabral <nani.andres@hotmail.com.ar>
2020-03-12 07:15:33 -0300

    exits

--- Encriptación/enc.py
+++ Encriptación/enc.py
@@ -5,7 +5,7 @@ Clave = "1234"
 def verificar(t):
     if t != Clave:
         ctypes.windll.user32.MessageBoxW(0, "Esa no es la clave maestra.", "Ups..", 0x10)
-        sys.exit()
+        return sys.exit()
     return True
 
 print("Ingresa la contraseña para proceder a desencriptar las cuentas.")
